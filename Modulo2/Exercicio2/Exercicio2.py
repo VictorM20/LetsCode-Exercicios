081bf0d3d03d79fca8f2ade66a78368955cb5cb4
Victor Mendes <vhs.mendes18@gmail.com>
2022-09-12 16:14:17 -0300

    Create Exercicio2.py

---
+++ Modulo2/Exercicio2/Exercicio2.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que peça para o usuário digitar uma palavra e imprima
+cada letra em uma linha.
+"""
+
+palavra = input('Digite uma palavra: ')
+
+for i in palavra:
+    print(i)
